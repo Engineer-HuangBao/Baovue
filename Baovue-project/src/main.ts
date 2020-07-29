23a895056db3b45ae91c52f73515a7cab2432623
黄宝 <2687255946@qq.com>
2020-07-29 18:48:58 +0800

    组件的挂载及引用

--- Baovue-project/src/main.ts
+++ Baovue-project/src/main.ts
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import components from "@/utils/components.ts";
 
 Vue.config.productionTip = false;
+
+// Vue.use 注入插件-(引入组件模块)
+Vue.use(components)
 
 new Vue({
   router,
---
+++ Baovue-project/src/utils/components.ts
@@ -0,0 +1,10 @@
+import HelloWorld from "@/components/hB-components/HelloWorld.vue";
+
+// vue组件的挂载
+function pluginComponents (Vue : any = {   Vue: ""  }) {
+  // 黄宝-----
+    Vue.component('HelloWorld', HelloWorld) //
+
+}
+
+export default pluginComponents
