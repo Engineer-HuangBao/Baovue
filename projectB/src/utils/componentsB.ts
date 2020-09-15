56fae2bc95a86de9dfabe5a0e6d01d8b03208b14
黄宝 <2687255946@qq.com>
2020-09-15 16:40:35 +0800

    从头来过

--- projectB/src/utils/componentsB.ts
+++
@@ -1,9 +0,0 @@
-import HelloWorld from "../components/HelloWorld.vue";
-
-// vue组件的挂载(写好的组件在这里引用)-组件引用合集
-function pluginComponents(Vue) {
-  // 黄宝-----公共组件封装合集
-  Vue.component("HelloWorld", HelloWorld); //
-}
-
-export default pluginComponents;
