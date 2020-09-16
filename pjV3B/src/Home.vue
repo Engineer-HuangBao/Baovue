d3119d6dcfb26be279e01cb7b23ef4d406f3e8b5
黄宝 <2687255946@qq.com>
2020-09-16 15:12:10 +0800

    路由雏形

---
+++ pjV3B/src/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
